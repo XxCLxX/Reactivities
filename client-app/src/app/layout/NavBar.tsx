6db8dc1c8551ef3f7185474aa80743cef4282c1a
Chikondi Luka <65278289+XxCLxX@users.noreply.github.com>
2021-12-09 18:44:47 +0700

    End of Section 17

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -24,7 +24,7 @@ export default observer(function NavBar() {
                     <Dropdown pointing="top left" text={user?.displayname}>
                         <Dropdown.Menu>
                             <Dropdown.Item as={Link}
-                                to={`/profile/${user?.username}`}
+                                to={`/profiles/${user?.username}`}
                                 text='My Profile'
                                 icon='user'
                             />
